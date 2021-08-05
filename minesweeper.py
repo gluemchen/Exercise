6d8257bbe4b909567527191925f473efe69afaac
Mike Jones <gluemchen81@googlemail.com>
2021-08-05 10:08:55 +0200

    trying to use variable instead of static value

--- minesweeper.py
+++ minesweeper.py
@@ -2,45 +2,71 @@
 import random
 
 
-def minesweeper(n, k):  # Function to create a minesweeper board
+def minesweeper(grid, bomb):  # Function to create a minesweeper board
     arr = [  # [0][1][2][3][4]*5
-        [0 for row in range(n)]  # fill defined range of row with Zero's
-        for column in range(n)  # start a column with 5 rows with defined range
+        [0 for row in range(grid)]  # fill defined range of row with Zero's
+        for column in range(grid)  # start a column with 5 rows with defined range
     ]
 
-    for num in range(k):
-        y = random.randint(0, n - 1)
-        x = random.randint(0, n - 1)
+    for num in range(bomb):
+        y = random.randint(0, grid - 1)
+        x = random.randint(0, grid - 1)
         arr[y][x] = "X"
 
         #                           0    1    2    3    4
-        if x >= 1 and x <= 3:  # I this case exists    0.['0']['?']['?']['?']['0']
-            arr[y][x + 1] += 1  # center right          0.['0']['0']['X']['-']['0']
-            arr[y][x - 1] += 1  # center left           0.['0']['-']['X']['0']['0']
+        if (x >= grid - 5 and x <= grid - 1) and (
+            y >= grid - 5 and y <= grid
+        ):  # I this case exists    0.['?']['?']['?']['?']['0']
+            if arr[y][x + 1] != "X":
+                arr[y][x + 1] += 1  # center right          0.['X']['X']['X']['X']['-']
 
-        if x == 0:
-            arr[y][x + 1] += 1  # center right            ['0']['0']['0']['0']['0']
+        if (x >= grid - 4 and x <= grid) and (
+            y >= grid - 5 and y <= grid
+        ):  # I this case exists    0.['0']['?']['?']['?']['?']
+            if arr[y][x - 1] != "X":
+                arr[y][x - 1] += 1  # center left           0.['-']['X']['X']['X']['X']
 
-        if x == 4:
-            arr[y][x - 1] += 1  # center left
+        # if x == 0:
+        #     arr[y][x + 1] += 1                         # center right            ['0']['0']['0']['0']['0']
 
-        if (x >= 1 and x <= 4) and (y >= 1 and y <= 4):  # top left
-            arr[y - 1][x - 1] += 1
+        # if x == 4:
+        #     arr[y][x - 1] += 1                         # center left
 
-        if (x >= 0 and x <= 3) and (y >= 1 and y <= 4):  # top right
-            arr[y - 1][x + 1] += 1
+        if (x >= grid - 4 and x <= grid - 1) and (
+            y >= grid - 4 and y <= grid - 1
+        ):  # top left
+            if arr[y - 1][x - 1] != "X":  # skip if X is already placed
+                arr[y - 1][x - 1] += 1
 
-        if (x >= 0 and x <= 4) and (y >= 1 and y <= 4):  # top center
-            arr[y - 1][x] += 1
+        if (x >= grid - 4 and x <= grid - 1) and (
+            y >= grid - 3 and y <= grid - 1
+        ):  # top right
+            if arr[y - 1][x + 1] != "X":  # skip if X is already placed
+                arr[y - 1][x + 1] += 1
 
-        if (x >= 0 and x <= 3) and (y >= 0 and y <= 3):  # bottom right
-            arr[y + 1][x + 1] += 1
+        if (x >= grid - 4 and x <= grid - 1) and (
+            y >= grid - 3 and y <= grid - 1
+        ):  # top center
+            if arr[y - 1][x] != "X":  # skip if X is already placed
+                arr[y - 1][x] += 1
 
-        if (x >= 1 and x <= 4) and (y >= 0 and y <= 3):  # bottom left
-            arr[y + 1][x - 1] += 1
+        if (x >= grid - 4 and x <= grid - 2) and (
+            y >= grid - 4 and y <= grid - 2
+        ):  # bottom right
+            if arr[y + 1][x + 1] != "X":  # skip if X is already placed
+                arr[y + 1][x + 1] += 1
 
-        if (x >= 0 and x <= 4) and (y >= 0 and y <= 3):  # bottom center
-            arr[y + 1][x] += 1
+        if (x >= grid - 3 and x <= grid) and (
+            y >= grid - 4 and y <= grid - 1
+        ):  # bottom left
+            if arr[y + 1][x - 1] != "X":  # skip if X is already placed
+                arr[y + 1][x - 1] += 1
+
+        if (x >= grid - 4 and x <= grid - 1) and (
+            y >= grid - 4 and y <= grid - 2
+        ):  # bottom center
+            if arr[y + 1][x] != "X":  # skip if X is already placed
+                arr[y + 1][x] += 1
 
     for row in arr:
         print(" ".join(str(cell) for cell in row))  #
